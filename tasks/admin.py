9a5cf6d9b8103d903fef7ec2332c3fd64055ae8c
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-09-30 18:32:02 +0300

    Registered models in admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import TaskType, Position, Worker, Team, Project, Task
 
-# Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ("workers",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("workers",)
+
+
+admin.site.register(TaskType, Project, Team)
